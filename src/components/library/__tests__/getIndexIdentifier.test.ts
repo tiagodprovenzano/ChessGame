b84896efc69c1ec44632f54635231f5faadf3dc1
Tiago <31420080+tiagodprovenzano@users.noreply.github.com>
2021-07-17 15:36:07 -0300

    standard position fixed

--- src/components/library/__tests__/getIndexIdentifier.test.ts
+++ src/components/library/__tests__/getIndexIdentifier.test.ts
@@ -2,21 +2,21 @@ import {getIndexIdentifier} from '../getIndexIdentifier'
 
 describe('checking if index returns correct space identifier', () => {
     it('if index === 0, returns A1', () => {
-        expect(getIndexIdentifier(0)).toBe('A1')
+        expect(getIndexIdentifier(0)).toBe('A8')
     })
     
     it('if index === 1, returns A2', () => {
-        expect(getIndexIdentifier(1)).toBe('A2')
+        expect(getIndexIdentifier(1)).toBe('A7')
     })
 
     it('if index === 8, returns B1', () => {
-        expect(getIndexIdentifier(8)).toBe('B1')
+        expect(getIndexIdentifier(8)).toBe('B8')
     })
 
     it('if index === 19, returns C3', () => {
-        expect(getIndexIdentifier(19)).toBe('C4')
+        expect(getIndexIdentifier(19)).toBe('C5')
     })
     it('if index === 28, returns D4', () => {
-        expect(getIndexIdentifier(28)).toBe('D5')
+        expect(getIndexIdentifier(28)).toBe('D4')
     })
 })
