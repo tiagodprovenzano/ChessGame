42d81d8807f1b5a1955ca5c5a9affe62f4cf6c3d
Tiago <31420080+tiagodprovenzano@users.noreply.github.com>
2021-07-18 08:11:39 -0300

    added pawn image

--- src/components/SpaceContainer.tsx
+++ src/components/SpaceContainer.tsx
@@ -1,48 +1,65 @@
-import React, { useContext, useEffect, useState } from "react";
-import { LayoutChangeEvent, Text, View, Image } from "react-native";
-import { getIndexIdentifier } from "./library/getIndexIdentifier";
-import { colorSelector } from "./library/colorSelector";
-import { Tile } from "./Tile";
-import { ChessContext } from "../utils/Context/ChessContext";
-import { IPositions } from "../utils/Chess/Board/types/IPositons";
-import { ITileType } from "../utils/Chess/Tiles/types/ITileType";
+import React, {useContext, useEffect, useState} from 'react';
+import {LayoutChangeEvent, Text, View, Image} from 'react-native';
+import {getIndexIdentifier} from './library/getIndexIdentifier';
+import {colorSelector} from './library/colorSelector';
+import {Tile} from './Tile';
+import {ChessContext} from '../utils/Context/ChessContext';
+import {IPositions} from '../utils/Chess/Board/types/IPositons';
+import {ITileType} from '../utils/Chess/Tiles/types/ITileType';
 
-const blackPawn = require("../../assets/blackPawn.png")
-const whitePawn = require("../../assets/whitePawn.png")
+const blackPawn = require('../../assets/blackPawn.png');
+const whitePawn = require('../../assets/whitePawn.png');
 
-export const SpaceContainer: React.FC<{ index: number; spaceWidth: number; }> = ({ index, spaceWidth }) => {
-    const [color] = useState<string>(colorSelector(index))
-    const [identifier] = useState<IPositions>(getIndexIdentifier(index))
-    const [tile, setTile] = useState<ITileType>()
-    const { Chess } = useContext(ChessContext)
+export const SpaceContainer: React.FC<{index: number; spaceWidth: number}> = ({
+  index,
+  spaceWidth,
+}) => {
+  const [color] = useState<string>(colorSelector(index));
+  const [identifier] = useState<IPositions>(getIndexIdentifier(index));
+  const [tile, setTile] = useState<ITileType>();
+  const {Chess} = useContext(ChessContext);
 
-    useEffect(() => {
-        if(Chess){
-            setTile(Chess.getPositionState(identifier))
-            
-        }
-    }, [Chess])
+  useEffect(() => {
+    if (Chess) {
+      setTile(Chess.getPositionState(identifier));
+    }
+  }, [Chess]);
 
-    return (
-        <View
-            key={identifier} 
-            testID={'SpaceContainer'}
-            style={{ 
-                width: spaceWidth,
-                height: spaceWidth, 
-                backgroundColor: color,
-                flex:0,
-                alignItems: 'center',
-                justifyContent: 'center' 
-            }} 
-        >
-            <Text testID={'SpaceIdentifierLabel'} style={{display: 'none', color: color === '#000'? '#fff': '#000'}}>{identifier}</Text>
-            <Text testID={'TileLabel'} style={{display: 'none', color: '#000'? '#fff': '#000'}}>{tile}</Text>
-            {
-
-               tile && <Image source={whitePawn} width={spaceWidth * 0.8} resizeMethod={'scale'} style={{width: spaceWidth * 0.7, height: spaceWidth * 0.7, alignSelf: 'center'}} />
-            }
-            <Tile />
-        </View>
-    );
+  return (
+    <View
+      key={identifier}
+      testID={'SpaceContainer'}
+      style={{
+        width: spaceWidth,
+        height: spaceWidth,
+        backgroundColor: color,
+        flex: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text
+        testID={'SpaceIdentifierLabel'}
+        style={{display: 'none', color: color === '#000' ? '#fff' : '#000'}}>
+        {identifier}
+      </Text>
+      <Text
+        testID={'TileLabel'}
+        style={{display: 'none', color: '#000' ? '#fff' : '#000'}}>
+        {tile}
+      </Text>
+      {tile && (
+        <Image
+          source={whitePawn}
+          width={spaceWidth * 0.8}
+          resizeMethod={'scale'}
+          style={{
+            width: spaceWidth * 0.7,
+            height: spaceWidth * 0.7,
+            alignSelf: 'center',
+          }}
+        />
+      )}
+      <Tile />
+    </View>
+  );
 };
